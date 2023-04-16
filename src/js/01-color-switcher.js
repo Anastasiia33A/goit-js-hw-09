641bedc37c49cc74af4721b1dfd0fe4619e20163
Anastasiia Akhmedkhanova <nastydoc@ukr.net>
2023-04-16 14:22:31 +0300

    1+

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let timerId = null;
+// не активна кнопка  Start 
+stopBtn.disabled = true;
+
+
+startBtn.addEventListener('click', () => {
+
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+    timerId = setInterval( () => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+})
+stopBtn.addEventListener('click', () => {
+
+    clearInterval(timerId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+})
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
